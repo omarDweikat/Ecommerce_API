e5c2b1f50ebe3d8a2393e8a1c34f4f96abe24c9a
Dardouk <ahmaddardouk12@outlook.sa>
2021-08-14 12:19:51 +0300

    ItemController

--- Features/Items/ItemController.cs
+++ Features/Items/ItemController.cs
@@ -17,8 +17,56 @@ namespace Ecommerce_API.Features.Items
             _context = context;
         }
          [HttpGet]
+        public async Task<IActionResult> GetItems(GetItems.Query query)
+        {
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+        //    [HttpGet]
+        // public async Task<IActionResult> GetItem(GetItem.Query query)
+        // {
+        //     var result = await _mediator.Send(query);
+        //     return Ok(result);
+        //  }
+         [HttpGet("{id}")]
         public async Task<IActionResult> GetItem(GetItem.Query query)
         {
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+          [HttpGet("{id}/images")]
+        public async Task<IActionResult> GetItemImages(GetItemImages.Query query)
+        {
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+          [HttpGet("SearchByName/{name}")]
+        public async Task<IActionResult> GetItemsByName(GetItemsByName.Query query)
+        {   
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+        [HttpGet("SearchByPrice/{MinPrice}/{MaxPrice}")]
+        public async Task<IActionResult> GetItemsByPrice(GetItemsByPrice.Query query)
+        {   
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+         [HttpGet("SearchByCategory/{Category}")]
+        public async Task<IActionResult> GetItemsByCategories(GetItemsByCategories.Query query)
+        {   
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+         [HttpGet("SearchByDate/{Date}")]
+        public async Task<IActionResult> GetItemsByDate(GetItemsByDate.Query query)
+        {   
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+        [HttpPost("uploadfile")]
+        public async Task<IActionResult> UploadImage(UploadImage.Query query)
+        {   
             var result = await _mediator.Send(query);
             return Ok(result);
         }
